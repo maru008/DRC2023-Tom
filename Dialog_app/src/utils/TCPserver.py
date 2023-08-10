c1a185c2e0b843a445239b8b210fe020312d9b0c
Naoki Yoshimaru <nydsa529422@gmail.com>
2023-08-10 10:05:53 +0900

    NLUServerのコネクションを逐次処理へ変更

--- Dialog_app/src/utils/TCPserver.py
+++ Dialog_app/src/utils/TCPserver.py
@@ -2,10 +2,12 @@ import socket
 
 class SocketConnection:
     def __init__(self, host='localhost', port=12345):
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s.connect((host, port))
+        self.host = host
+        self.port = port
 
     def send_data(self, data):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((self.host, self.port))
         encoded_data = data.encode('utf-8', errors='ignore')
-        self.s.sendall(encoded_data)
-        # print(f"Sent data: {data}")
+        s.sendall(encoded_data)
+        s.close()  # 送信後にソケットを閉じる
--- NLUServer_app/src/utils/receive_data.py
+++ NLUServer_app/src/utils/receive_data.py
@@ -6,15 +6,6 @@ class Server:
         self.port = port
         self.server = None
         self.conn = None
-
-    def start_server(self):
-        print("Starting server...")
-        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.bind((self.host, self.port))
-        self.server.listen()
-        print(f"Server started on {self.host}:{self.port}. Waiting for connections...")
-        self.conn, addr = self.server.accept()
-        print(f'Connected by {addr[0]}:{addr[1]}')
         
     def start_server(self):
         print("Starting server...")
@@ -25,16 +16,14 @@ class Server:
 
     def accept_connections(self):
         while True:
-            self.conn, addr = self.server.accept()  # 新しい接続を受け入れる
+            print("="*100)
+            conn, addr = self.server.accept()  # 新しい接続を受け入れる
             print(f'Connected by {addr[0]}:{addr[1]}')
-            while True:  # このループで同一接続からのデータを受け取り続ける
-                data = self.receive_data(self.conn)
-                if data:
-                    return data
-                else:  # dataがNoneの場合、接続が閉じられたと判断
-                    break
-            self.conn.close()  # 接続を閉じる
-            print(f"Connection from {addr[0]}:{addr[1]} closed.")
+            data = self.receive_data(conn)  # データを受け取る
+            if data:
+                conn.close()  # 接続を閉じる
+                print(f"Connection from {addr[0]}:{addr[1]} closed.")
+                return data
 
     def receive_data(self, conn):
         data = conn.recv(1024)
