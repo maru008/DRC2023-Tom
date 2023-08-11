48adb739edfb202e9b3c1f40258d77edd1354836
Naoki Yoshimaru <nydsa529422@gmail.com>
2023-08-11 15:49:33 +0900

    RURUBUの観光地データの交通情報から取得するNLUして構造化データを取得するコードを作成

--- CityGraph_app/src/data_preparation.py
+++ CityGraph_app/src/data_preparation.py
@@ -7,6 +7,8 @@ import requests
 import json
 import os
 import googlemaps
+
+from NLUModule.NLU_text import NLU
 # =====================================================================================================================
 #APIキーなどの定義
 # =====================================================================================================================
@@ -21,13 +23,14 @@ config_obj = read_config()
 RURUBU_API_KEY = "appid=" + str(config_obj.get("API_Key", "RURUBU"))
 GOOGLE_API_KEY = str(config_obj.get("API_Key","Google"))
 
+output_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../data/output_data/')
 # =====================================================================================================================
-#ルルブAPIから観光地データを取り出して，データを保存する
+#るるぶAPIから観光地データを取り出して，データを保存する
 # =====================================================================================================================
 # 出力のパス設定
-save_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../data/output_data/RURUBU_results_KyotoCity.json')
+RURUBU_save_path = os.path.join(output_data_path, 'RURUBU_results_KyotoCity.json')
 # ファイルが存在するかチェック
-if os.path.exists(save_path):
+if os.path.exists(RURUBU_save_path):
     print("RURUBUAPIから取得した観光地ファイルがすでに存在します。スキップします。")
 else:
     urlbase = "https://www.j-jti.com/appif/sight?"
@@ -58,17 +61,65 @@ else:
                 all_results.extend(add_ls)
 
     # JSONファイルとして保存
-    with open(os.path.join(save_path, 'RURUBU_results_KyotoCity.json'), 'w',encoding='utf-8') as file:
-        json.dump(all_results, file, ensure_ascii=False)
+    with open(RURUBU_save_path, 'w',encoding='utf-8') as file:
+        json.dump(all_results, file, ensure_ascii=False, indent=4)
 
     print(f"保存された観光地数：{len(all_results)}")
+
+# =====================================================================================================================
+#るるぶAPIから取り出された観光地データから交通情報をNLUする
+# =====================================================================================================================
+RURUBU_TraficInfo_save_path = os.path.join(output_data_path, 'RURUBU_KyotoCity_TraficInfo.json')
+
+# もしファイルが存在するならデータをロード
+if os.path.exists(RURUBU_TraficInfo_save_path):
+    with open(RURUBU_TraficInfo_save_path, 'r', encoding='utf-8') as file:
+        existing_data = json.load(file)
+else:
+    existing_data = []
+
+with open(RURUBU_save_path, 'r', encoding='utf-8') as f:
+    RURUBU_res = json.load(f)
+
+if len(RURUBU_res) == len(existing_data):
+    print("RURUBUデータからNLUしたファイルがすでに完成してます。スキップします。")
+else:
+    # NLUモジュールの準備
+    NLU_module = NLU(config_obj)
+    prompt_path = os.path.join("./NLUModule/Prompts/Get_Trafic_info.txt")
+    with open(prompt_path, 'r', encoding='utf-8') as f:
+        prompt_text = f.read()
     
+    existing_sight_ids = [entry["SightID"] for entry in existing_data]
+    
+    # ここから観光地ごとの処理
+    for spot_i in tqdm(RURUBU_res,desc="NLU TraficInfo"):
+        SightID = spot_i["SightID"]
+        if SightID in existing_sight_ids:
+            continue
+        try:
+            Traffic2 = spot_i["Traffic2"] if "Traffic2" in spot_i else ""
+            TraficInfoText = spot_i["Traffic1"] +"/" +Traffic2+"/最寄り駅:" + spot_i['Station']["Name"]
+            res_json = json.loads(NLU_module.NLU_GPT4(TraficInfoText, prompt_text, []))
+            res_json["SightID"] = SightID
+            existing_data.append(res_json)
+        except Exception as e:
+            print(f"エラーが発生しました: {e}")
+            with open(RURUBU_TraficInfo_save_path, 'w', encoding='utf-8') as file:
+                json.dump(existing_data, file, ensure_ascii=False, indent=4)
+            raise
+
+    with open(RURUBU_TraficInfo_save_path, 'w', encoding='utf-8') as file:
+        json.dump(existing_data, file, ensure_ascii=False, indent=4)
+
+    print("RURUBUAPIの交通情報を構造化したデータがRURUBU_KyotoCity_TraficInfo.jsonファイルに保存されました。")
+
 # =====================================================================================================================
 #京都市内の駅を取り出して，緯度経度を計算，データを保存する
 # =====================================================================================================================
-save_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../data/output_data/stations_geocode.json')
+Geocode_save_path = os.path.join(output_data_path, 'KyotoCity_Stations_geocode.json')
 
-if os.path.exists(save_path):
+if os.path.exists(Geocode_save_path):
     print("GoogleAPIから取得した駅Geocodeファイルがすでに存在します。スキップします。")
 else:
     gmaps = googlemaps.Client(key=GOOGLE_API_KEY)
@@ -96,7 +147,7 @@ else:
         }
 
     # JSONファイルに保存
-    with open('stations_geocode.json', 'w', encoding='utf-8') as file:
+    with open(Geocode_save_path, 'w', encoding='utf-8') as file:
         json.dump(result, file, ensure_ascii=False, indent=4)
 
     print("緯度と経度が stations_geocode.json ファイルに保存されました。")
--- README.md
+++ README.md
@@ -4,6 +4,8 @@ in DRC2023, Team Tom
 ## 環境
 - Python
 - Docker
+- Unix系(mac or Ubuntu)
+  
 ### 環境設定
 対話メインプログラムはpythonを用います．
 仮想環境を作成することを推奨．
@@ -19,15 +21,21 @@ pip install -r Dialog_app/requirements.txt
 対話プログラム（start_dialog.sh）は以降この環境で実行．
 ---
 ## 設定項目
-APIキーやサーバホストを各自の設定ファイルに作成する．
+APIキーやサーバホストを各自の設定ファイルに作成する．APIは以下が必要です．
+- OpenAI
+- RURUBU
+- Google
+  
 以下のパスのようにconfig.iniファイルを作成する．
 - config.ini
-  
+
 形式は以下.(適宜変更する)
+
 ```
 [API_Key]
 OpenAI = *************
 RURUBU = *************
+GOOGLE = *************
 
 [Server_Info]
 Server_ip = 192.168.2.213
@@ -38,59 +46,19 @@ RobotBodyController_port = 21000
 FaceRecognition_port = 4500
 ```
 
+---
+## 前準備
+地理データを準備する必要があります．
+具体的には以下のデータ処理を行います．
+
+```
+python CityGraph_app/src/data_preparation.py
+```
+
+---
+## コンテナ起動
 実行は以下のシェルファイルを実行する．
 - start_server.sh
 - start_dialog.sh
-  
-コマンド対話モードを使用しますか？(y/n)を聞かれるので，会話だけをしたい時はy．サーバ接続をして試したい時はnを入力する．
 
----
-## フォルダ構成
-このプロジェクトは以下の構成で作成されている．
-```
-.
-├── Dialog_app
-│   ├── requirements.txt
-│   └── src
-│       ├── DialogModules
-│       │   ├── NLGModule.py
-│       │   └── Prompts
-│       │       └── Dialog_staff.txt
-│       ├── ServerModules
-│       │   ├── expression_generation.py
-│       │   ├── motion_generation.py
-│       │   ├── speech_generation.py
-│       │   └── voice_recognition.py
-│       ├── database
-│       │   └── mongo_tools.py
-│       ├── main.py
-│       └── utils
-│           ├── TCPserver.py
-│           ├── config_reader.py
-│           └── general_tool.py
-├── LICENSE
-├── MongoDB
-│   ├──...
-│
-├── NLUServer_app
-│   ├── Dockerfile
-│   ├── config.ini
-│   ├── requirements.txt
-│   └── src
-│       ├── NLUModule
-│       │   ├── Prompts
-│       │   │   └── GPT4_NLU.txt
-│       │   ├── Text_NLU_module.py
-│       │   └── __pycache__
-│       │       └── Text_NLU_module.cpython-39.pyc
-│       ├── main.py
-│       └── utils
-│           ├── config_reader.py
-│           ├── mongodb_tool.py
-│           └── receive_data.py
-├── README.md
-├── config.ini
-├── docker-compose.yml
-├── start_dialog.sh
-└── start_server.sh
-```
+コマンド対話モードを使用しますか？(y/n)を聞かれるので，会話だけをしたい時はy．サーバ接続をして試したい時はnを入力する．
